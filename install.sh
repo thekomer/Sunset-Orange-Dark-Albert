794f24bf43addd163b61595574d79a681454918b
Komer <thekomer@gmail.com>
2024-11-19 04:24:36 +0100

    BUGfix: install script accepting "e" as "yes".

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ THEMESLOCATION="/usr/share/albert/widgetsboxmodel/themes/"
 read -p "Install Albert Sunset Orange theme? Answer Yes/No `echo $'\n> '`" yn
 
 case $yn in
-	[Yesyes]* )
+	Yes|yes)
         	# If the user selected yes, Lets copy the theme files over
 		echo -e "${RED}  Deleting old theme file: ${NOCOLOUR}"
 		echo -e "${BLUE}  sudo rm -f ${THEMESLOCATION}Sunset-Orange-Dark-Albert.qss ${NOCOLOUR}"
@@ -17,7 +17,7 @@ case $yn in
 		sudo cp ./theme/Sunset-Orange-Dark-Albert.qss ${THEMESLOCATION}
 					# Thank the user ~
 		echo -e "${RED}  Theme has been installed to [Albert themes folder] - Thank you! :-) ${NOCOLOUR}";;
-	[Nono]* )
+	No|no)
 		echo -e "${BLUE}  You've chosen not to install the theme. :-(${NOCOLOUR}";;
-	* ) echo "Please answer yes or no.";;
+	*) echo "Please answer Yes or No.";;
 esac
